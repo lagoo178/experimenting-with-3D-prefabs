7677a978635c962c582bf2f6a2cca8398083a20d
Sutan Daffa Satria Hertanto <lagooo178@gmail.com>
2021-11-14 21:39:04 +0700

    update

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Im learning unity engine");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
